2ee866c055d44eac7e44f8b2c070d04561575f3d
fajlerabbi-dev <fajlerabbi827@gmail.com>
2022-04-22 15:32:21 +0600

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { CreateProfile, UserLogin } = require('../controller/ProfileController');
+const AuthVerifyMiddleware = require('../middleware/AuthVerifyMiddleware');
 const router = express.Router();
 
 
 // API ROUTES
 router.post('/CreateProfile', CreateProfile)
 router.post('/UserLogin', UserLogin);
+router.post('/SelectProfile', AuthVerifyMiddleware, UserLogin);
 
 
 module.exports = router;
